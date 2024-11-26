89ab02efe1c4e3bc21e67c312d8ff7f40c1d6cff
Vamelyre <168769184+Vamelyre@users.noreply.github.com>
2024-11-26 12:12:35 +0400

    Create einstein.py

---
+++ problem_set1/einstein.py
@@ -0,0 +1,11 @@
+ def main():
+    c = 300000000
+    
+    mass = int(input("enter mass in k: "))
+    
+    energy = mass * c * c
+    
+    print(f"the equivalent energy is {energy} joules.")
+
+if __name__ == "__main__":
+    main()
